bc0addd142152a1ba36aa5ae5856d26b9968e870
Elainax <1578349179@qq.com>
2026-05-14 09:29:25 +0800

    更改training的设置

--- zxyRunAll.sh
+++ zxyRunAll.sh
@@ -1,11 +1,11 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 python train.py \
 -s ~/autodl-tmp/data/mipnerf360/room  \
--m ~/autodl-tmp/output/models/room \
+-m ~/autodl-tmp/output/models/exp_0.1.0/room \
 --quiet \
 --eval \
 --indoor \
---solidification_start 20000 \
+--solidification_start 1000 \
 --lambda_conn_max 0.01 \
 --lambda_opaque_max 0.005 \
 --lambda_area 1e-4 \
@@ -13,5 +13,5 @@ python train.py \
 --k_edge 5
 
 
-python render.py  -m ~/autodl-tmp/output/models/room
-python metrics.py -m ~/autodl-tmp/output/models/room
+python render.py  -m ~/autodl-tmp/output/models/exp_0.1.0/room
+python metrics.py -m ~/autodl-tmp/output/models/exp_0.1.0/room
