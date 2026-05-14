eebf7f5ae74698ec6afe9b132b4274d5376be9dc
Elainax <1578349179@qq.com>
2026-05-14 22:27:30 +0800

    关闭论文原本带的delaunay变换

--- zxyRunAll.sh
+++ zxyRunAll.sh
@@ -21,7 +21,8 @@ python train.py \
 --max_points 1000000 \
 --final_opacity_iter 24000 \
 --sigma_until 30000 \
---iteration_mesh 5000
+--iteration_mesh 5000 \
+--skip_delaunay
 
 
 python render.py  -m ~/autodl-tmp/output/models/train02/room
